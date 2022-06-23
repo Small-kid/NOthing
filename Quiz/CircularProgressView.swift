632c6eb978e0f78fddbcb1f3c2173b237b004715
Jun Heng <zudoturiku@duck.com>
2022-06-23 14:44:19 +0800

    Milestone 5: Circular Progress View (Jun Heng)

---
+++ Quiz/CircularProgressView.swift
@@ -0,0 +1,32 @@
+//
+//  CircularProgressView.swift
+//  Quiz
+//
+//  Created by Wong Jun heng on 23/6/22.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    var progress: CGFloat
+    var body: some View {
+        ZStack{
+            Circle()
+                .stroke(lineWidth:20)
+                .opacity(0.4)
+                .foregroundColor(.black)
+            
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(style: .init(lineWidth:20.0, lineCap: .round ,
+                    lineJoin: .round))
+                .foregroundColor(.blue)
+        }
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProgressView(progress: 0.25)
+    }
+}
