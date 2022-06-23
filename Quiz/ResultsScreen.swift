072af652a78c5cb1dc3ce188c8239e185f3c0011
Du Yuhan <yuhan.du@gmail.com>
2022-06-23 15:29:41 +0800

    Commit 6: Fixed aggressive summary

--- Quiz/ResultsScreen.swift
+++ Quiz/ResultsScreen.swift
@@ -19,31 +19,41 @@ struct ResultsScreen: View {
              .frame(width: 150, height: 150)
             Text("Congrats! You got")
             Text("\(score)/\(totalQuestions)")
+            
             if score == 0 {
                 Text("Aww man go google a link to watch paw patrol free online (pirating ahem)")
-                    .foregroundColor(.white)
+                    .foregroundColor(.black)
                     .padding()
-            }
-            if score == 1 {
-                Text("Aww man go google a link to watch paw patrol free online (pirating ahem)")
-                    .foregroundColor(.white)
-                    .padding()
-            }
-            if score == 2{
-                Text("You passed, congratulations - but you still kinda suck :/")
-                    .foregroundColor(.white)
-                    .padding()
-            }
-            if score == 3{
-                Text("THE FATAL FLAW IN YOUR SEEMINGLY COMPLETE KNOWLEDGE HAS SURFACED GO TO THE WIKI AND PATCH IT UP - YOU WERE SO CLOSE TO PERFECTION SIGH")
-                    .foregroundColor(.white)
-                    .padding()
+                    .multilineTextAlignment(.center)
 
             }
-            if score == 4{
-                Text("THE FATAL FLAW IN YOUR SEEMINGLY COMPLETE KNOWLEDGE HAS SURFACED GO TO THE WIKI AND PATCH IT UP - YOU WERE SO CLOSE TO PERFECTION SIGH")
-                    .foregroundColor(.white)
+            else if score == 1 {
+                Text("Aww man go google a link to watch paw patrol free online (pirating ahem)")
+                    .foregroundColor(.black)
                     .padding()
+                    .multilineTextAlignment(.center)
+
+            }
+            else if score == 2{
+                Text("You passed, congratulations - but you still kinda suck :/")
+                    .foregroundColor(.black)
+                    .padding()
+                    .multilineTextAlignment(.center)
+
+            }
+            else if score == 3{
+                Text("THE FATAL FLAW IN YOUR SEEMINGLY COMPLETE KNOWLEDGE HAS SURFACED GO TO THE WIKI AND PATCH IT UP - YOU WERE SO CLOSE TO PERFECTION SIGH")
+                    .foregroundColor(.black)
+                    .padding()
+                    .multilineTextAlignment(.center)
+
+
+            }
+            else if score == 4{
+                Text("THE FATAL FLAW IN YOUR SEEMINGLY COMPLETE KNOWLEDGE HAS SURFACED GO TO THE WIKI AND PATCH IT UP - YOU WERE SO CLOSE TO PERFECTION SIGH")
+                    .foregroundColor(.black)
+                    .padding()
+                    .multilineTextAlignment(.center)
             }
         }
     }
